03069f495b4ac1f9a610d739f7b26dd632d9a6db
Caleb Aviles <earthctzn1@gmail.com>
2019-05-07 13:22:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+  def roster
+     roster = {}
+  end
+  def add_student(student)
+    @student = student
+    roster << student 
+  end
+end 
