3e7bd2e752023f75e518e7f419de18304aaf697c
Caleb Aviles <earthctzn1@gmail.com>
2019-05-08 00:07:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,10 +6,8 @@ class School
      roster = {}
   end
   def add_student(student, att)
-    @student = student
-    @att = att
-    roster[@att] = []
-    roster[@att] << @student
+    roster[att] = []
+    roster[att] << student
     
   end
 end 
