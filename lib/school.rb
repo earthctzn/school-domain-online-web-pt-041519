7b2bf70e68bf8174f192a74a02f9bc9d0a01f315
Caleb Aviles <earthctzn1@gmail.com>
2019-05-08 00:13:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,9 +2,9 @@ class School
   def initialize(name)
     @name = name
   end
-  def roster
-     roster = {}
-  end
+  
+  ROSTER = {}
+  
   def add_student(student, att)
     roster[att] = [] if !att.include?
     roster[att] << student
